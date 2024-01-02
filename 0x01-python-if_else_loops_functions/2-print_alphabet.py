cfc0871b00db513e7449ce10f49eda9a4b642dfd
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-01-02 15:54:17 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for letter in "abcdefghijklmnopqrstuvwxyz":
-    print("{}".format(chr(letter)), end="");
+"""abcdefghijklmnopqrstuvwxyz"""
+for i in range(97, 123, 1):
+    print("{}".format(chr(i)), end="");
