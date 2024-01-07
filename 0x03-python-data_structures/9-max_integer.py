604b928a1f26188c811411412ea3c590cfc1885e
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-01-07 07:19:50 -0800

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    A = my_list[0]
+    for a in range(len(my_list)):
+        if my_list[a] > big:
+            A = my_list[i]
+    return (A)
