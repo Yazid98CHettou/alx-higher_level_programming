0babbeebd53d8a29a27158ae64d03e5c2e9e3d18
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-01-10 11:22:42 -0800

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#/!usr/bin/pyhton3
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for a in range(len_list)):
+        if new_list[a] == search:
+            new_list[a] = replace
+    return (new_list)
