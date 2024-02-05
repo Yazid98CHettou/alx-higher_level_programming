cb64295f6a765ae393c6bef89eaafcbaa5cd7599
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-02-05 15:46:15 -0800

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """module with method inherits_from"""
+
+
 def inherits_from(obj, a_class):
     """Method that return True if an object is an instance of a class
     that inherited from"""
