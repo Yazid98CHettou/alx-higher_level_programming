511d6745c9b9c11a89ebecd5612a4b91c36c61e0
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-02-06 17:29:12 -0800

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an objec"""
+"""returns the dictionary description with simple data structure"""
 
 
 def class_to_json(obj):
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""class Student that defines a student by:Public instance attributes:first_name.last_name.age"""
+"""class Student that defines a student"""
 
 
 class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initialize a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
