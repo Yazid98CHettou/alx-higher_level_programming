a38aa7aa0959d8db960cb086693e9ead3e9c8fac
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-02-06 16:49:47 -0800

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""function that reads a text file (UTF8) and prints it to stdout"""
+"""Module containing the function read_file"""
+
+
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """Reads a file and prints to stdout"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
