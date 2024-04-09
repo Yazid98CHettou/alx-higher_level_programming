6e0b84c0fa7998d0b6db8df8653295f6a5542c15
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-08 18:34:07 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-line.forEach(function (el) {
-  console.log(el);
-});
+console.log('C is fun\nPython is cool\nJavascript is amazing');
