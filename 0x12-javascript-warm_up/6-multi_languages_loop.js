29bdeefe0443031e0cb39ae39f16fb381cb34119
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-08 18:30:47 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < 3; i++) {
-  console.log(arr[i]);
+  console.log(myVar[i]);
 }
