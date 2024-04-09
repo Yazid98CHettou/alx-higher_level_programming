19148aaa02c9c259dc4bb472d270199960e6247c
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-08 18:33:10 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args.length === 3) {
-  console.log('Argument found');
-} else if (arg.length > 3) {
-  console.log('Arguments found');
-} else {
+if (process.argv.length === 2) {
   console.log('No argument');
+}
+if (process.argv.length === 3) {
+  console.log('Argument found');
+}
+if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
