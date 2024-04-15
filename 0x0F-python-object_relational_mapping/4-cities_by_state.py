0d27066d82ad6968c729efbda102ada3c5195426
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-14 17:28:40 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 if __name__ == '__main__':
     data = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], data=sys.argv[3])
-    curs = db.cursor()
+    curs = data.cursor()
     curs.execute("SELECT cities.id, cities.name, states.name FROM cities \
                 JOIN states ON cities.state_id = states.id \
                 ORDER BY cities.id")
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 if __name__ == '__main__':
     data = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], data=sys.argv[3])
-    curs = db.cursor()
+    curs = data.cursor()
     curs.execute("SELECT * FROM cities JOIN states \
                 ON cities.state_id = states.id ORDER BY cities.id")
     [print(", ".join([c[2] for c in curs.fetchall() if c[4] == sys.argv[4]]))]
