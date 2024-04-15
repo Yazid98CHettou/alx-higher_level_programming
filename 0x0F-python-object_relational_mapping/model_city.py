947787372fe3036274ba7317b817dfbdc76d628d
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-14 17:43:45 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,5 +5,5 @@ Base = declarative_base()
 class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,5 +4,5 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True)
