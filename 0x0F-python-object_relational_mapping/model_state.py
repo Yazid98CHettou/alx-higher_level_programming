b4ff4a967046c1b15a4e6971d879ae93190a952c
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-18 16:49:35 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-""" Model City """
-from sqlalchemy import Column, Integer, String, ForeignKey
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
 
 
 class City(Base):
-    """ Class City inherith from Base
-    Implement model cities """
+
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True,
-                unique=True, nullable=False,
-                autoincrement=True)
-    state_id = Column(Integer, ForeignKey('states.id'),
-                      nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
