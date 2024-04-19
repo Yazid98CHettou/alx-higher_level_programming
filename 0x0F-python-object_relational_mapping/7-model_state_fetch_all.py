ccddb733c399e37fedf148d62a8562a000bf2de0
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-18 17:04:57 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,10 +1,8 @@
 i#!/usr/bin/python3
 import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
