fa26340785bdfd09b43635f9fb3462793a57fce9
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-26 15:20:18 -0700

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
+import urllib.request as url
 if __name__ == "__main__":
-    import urllib.request as url
-    req = url.Request('https://intranet.hbtn.io/status')
+    Rqst = url.Request('https://intranet.hbtn.io/status')
     with url.urlopen(req) as response:
         html = response.read()
         print("Body response:")
