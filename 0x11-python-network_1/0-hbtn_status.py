11bd5ba46ca61b3667d9157317355c91e279d21b
Yazid98CHettou <ch.yazid1998.yc@gmail.com>
2024-04-26 15:00:05 -0700

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
-import urllib.request
-
-if __name__ == '__main__':
-
-    Rqst = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page))
-        print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
+if __name__ == "__main__":
+    import urllib.request as url
+    req = url.Request('https://intranet.hbtn.io/status')
+    with url.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf-8'))
